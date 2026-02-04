1bbcb1f9ea7cdfeed19525a7289e660ce830ed28
Aeros72 <pashamelnyk2004@gmail.com>
2026-02-05 01:03:29 +0200

    Fixed URL routing for user

--- api_service_theatre/urls.py
+++ api_service_theatre/urls.py
@@ -27,7 +27,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
-    path("api/user", include("user.urls", namespace="user")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/swagger/", SpectacularSwaggerView.as_view(), name="swagger"),
     path("api/doc/redoc/", SpectacularRedocView.as_view(), name="redoc"),
